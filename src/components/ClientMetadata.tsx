603c48ae1d8a524cf20faad9be1e3579b9d0ef70
AnthonyBarbaro <anthonybarbaro40@gmail.com>
2025-03-14 13:58:47 -0700

    About Page mobile friendly, added Analytics

--- src/components/ClientMetadata.tsx
+++ src/components/ClientMetadata.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function ClientMetadata() {
-  return <SpeedInsights />;
+  return (
+    <>
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
