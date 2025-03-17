42b22f9c84b023877e8bfae47e60e78855f450f5
AnthonyBarbaro <anthonybarbaro40@gmail.com>
2025-03-17 10:16:34 -0700

    Sitemap, Products and Categories Get pulled for SEO

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
